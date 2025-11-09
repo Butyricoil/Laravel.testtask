8c0914afef57692486e31f4df17d36f49943344f
butyricoil <butyricoil@gmail.com>
2025-11-09 18:21:23 +0400

    Middleware change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(remove: [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+        $middleware->api(append: [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
